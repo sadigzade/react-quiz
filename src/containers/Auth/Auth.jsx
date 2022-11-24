1dae241d893db854b178ce180e933958947b064f
Хикмет Садигзаде <sadigzade.hi@mail.ru>
2022-11-24 16:19:13 +0300

    Created an authorization page

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
+import Button from '../../components/UI/Button/Button';
+
+import classes from './Auth.module.scss';
+
 class Auth extends Component {
+  loginHandler = () => {};
+
+  registerHandler = () => {};
+
+  submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            <input type="text" />
+            <input type="text" />
+
+            <Button type="success" onClick={this.loginHandler}>
+              Войти
+            </Button>
+            <Button type="primary" onClick={this.registerHandler}>
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
