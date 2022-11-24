9ac3074e3ddc46ffa91d069f9b2558b74b0df6f6
Хикмет Садигзаде <sadigzade.hi@mail.ru>
2022-11-24 22:20:23 +0300

    Created a test creation page

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
+import Button from '../../components/UI/Button/Button';
+
+import classes from './QuizCreator.module.scss';
+
 class QuizCreator extends Component {
+  onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  handleAddQuestion = () => {};
+
+  handleCreateQuiz = () => {};
+
   render() {
     return (
-      <div>
-        <h1>QuizCreator</h1>
+      <div className={classes.QuizCreator}>
+        <div>
+          <h1>Создание теста</h1>
+          <form onSubmit={this.onSubmitHandler}>
+            <input type="text" />
+            <hr />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+
+            <select></select>
+            <Button type="primary" onClick={this.handleAddQuestion}>
+              Добавить вопрос
+            </Button>
+            <Button type="success" onClick={this.handleCreateQuiz}>
+              Создать тест
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
